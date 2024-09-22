7b70cfae6ca3dc4b21f02ff006832a08b4542260
baraneldemir <brneldemirrr@gmail.com>
2024-09-22 22:43:29 +0300

    change

--- src/ContactUsPage.jsx
+++ src/ContactUsPage.jsx
@@ -7,12 +7,12 @@ export default function ContactUsPage() {
           <div class="w-full px-4">
             <div class="mx-auto mb-[60px] max-w-[520px] text-center lg:mb-20">
               <span class="block mb-2 text-lg font-semibold text-primary">
-                FAQ
+                FAQ I love Rosie ehehe
               </span>
               <h2
                 class="text-dark dark:text-white mb-4 text-3xl font-bold sm:text-[40px]/[48px]"
               >
-                I Love you rosie ehehe
+                .....
               </h2>
               <p class="text-base text-body-color dark:text-dark-6">
                 
