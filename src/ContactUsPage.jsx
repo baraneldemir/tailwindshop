f19a67ad37f3e7b4142387f20b364cc269613efc
baraneldemir <brneldemirrr@gmail.com>
2024-09-25 19:37:27 +0300

    size selection and related ui added 25 sept commit

--- src/ContactUsPage.jsx
+++ src/ContactUsPage.jsx
@@ -7,7 +7,7 @@ export default function ContactUsPage() {
           <div className="w-full px-4">
             <div className="mx-auto mb-[60px] max-w-[520px] text-center lg:mb-20">
               <span className="block mb-2 text-lg font-semibold text-primary">
-                FAQ I love Rosie ehehe
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias dolores atque quia possimus at officiis ex excepturi voluptatum, natus autem, velit dolorum. Velit quidem, earum non vel illo esse cupiditate.
               </span>
               <h2
                 className="text-dark dark:text-white mb-4 text-3xl font-bold sm:text-[40px]/[48px]"
