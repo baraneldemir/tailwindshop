5963efbe9685c0503c29d096d618a0d402e70bae
baraneldemir <brneldemirrr@gmail.com>
2024-09-28 22:24:49 +0300

    Slide landing page added 28 sept commit

--- src/pages/PlantHoodiePage.jsx
+++ src/pages/PlantHoodiePage.jsx
@@ -5,12 +5,16 @@ import StoreItem from '../components/StoreItem';
 
 export default function PlantHoodiePage() {
   return (
+    <div>
+      <h1 className="ml-4 text-2xl text-black ">Our Products</h1>
     <div className="block mb-48">
+      
       <div className="grid grid-cols-2 gap-4 mx-3 xl:grid-cols-4">
         {storeItems.map((item, index) => (
           <StoreItem key={index} {...item} />
         ))}
       </div>
+    </div>
     </div>
   );
 }
